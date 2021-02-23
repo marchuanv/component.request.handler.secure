c4d716307be55edf1555c0f628830a860b891adc
marchuanv <marchuanv@gmail.com>
2021-02-23 23:18:00 +0200

    refactor

--- component.request.handler.secure.authenticate.js
+++ component.request.handler.secure.authenticate.js
@@ -11,13 +11,20 @@ module.exports = {
         }
         const name = `${options.port}${options.path}`;
         requestHandlerUser.handle(options);
+        const isPassphraseProtected = (
+            ( options.hashedPassphrase !== null && options.hashedPassphrase !== "" && options.hashedPassphrase !== undefined) &&
+            ( options.hashedPassphraseSalt !== null && options.hashedPassphraseSalt !== "" && options.hashedPassphraseSalt !== undefined)
+        );
         //This is a passthrough the component.request.handler.secure component needs to check the headers for security and decide
         delegate.register(`component.request.handler.secure.authenticate`, name, async ({ session, headers, data }) => {
             let { passphrase, encryptionkey, token } = headers;
             delete headers["passphrase"];
             delete headers["encryptionkey"];
             delete headers["token"];
-            if (passphrase){
+            if (session.encryptionkey && !session.encryptionkey.remote){
+                session.encryptionkey.remote = encryptionkey;
+            }
+            if (isPassphraseProtected && passphrase){
                 const results = utils.hashPassphrase(passphrase, options.hashedPassphraseSalt);
                 if (results.hashedPassphrase ===  options.hashedPassphrase){
                     logging.write("Request Handler Secure Authenticate",`session ${session.Id} is authenticated.`);
@@ -30,32 +37,35 @@ module.exports = {
                         remote: encryptionkey
                     };
                     session.hashedPassphrase = results.hashedPassphrase;
+                    token = session.token;
+                } else {
+                    return {
+                        headers: { 
+                            "Content-Type":"text/plain"
+                        },
+                        statusCode: 401,
+                        statusMessage:"Unauthorised",
+                        data: "passphrase required"
+                    };
                 }
-                return {
-                    headers: { 
-                        "Content-Type":"text/plain",
-                        token: session.token,
-                        encryptionkey: session.encryptionkey.local
-                    },
-                    statusCode: 200,
-                    statusMessage:"Authorised",
-                    data: ""
-                };
+               
             }
-            if (session.token !== token){
+            if (isPassphraseProtected && (!passphrase && !token) || (token && session.token !== token)){
                 return {
                     headers: { 
                         "Content-Type":"text/plain"
                     },
                     statusCode: 401,
                     statusMessage:"Unauthorised",
-                    data: ""
+                    data: "passphrase or token required"
                 };
             }
-            if (session.encryptionkey && !session.encryptionkey.remote){
-                session.encryptionkey.remote = encryptionkey;
+            const res = await delegate.call({ context: "component.request.handler.secure", name }, { session, data });
+            if (res.headers){
+                res.headers.token = session.token;
+                res.headers.encryptionkey = session.encryptionkey.local;
+                return res;
             }
-            return await delegate.call({ context: "component.request.handler.secure", name }, { session, headers, data });
         });
     }
 };
