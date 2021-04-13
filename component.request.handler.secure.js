d3a5b8134cc4234fbfc7d13dff6a7518defffb8a
marchuanv <marchuanv@gmail.com>
2021-04-14 00:21:30 +0200

    refactor

--- component.request.handler.secure.js
+++ component.request.handler.secure.js
@@ -2,83 +2,111 @@ const utils = require("utils");
 const component = require("component");
 component.load(module).then( async ({ requestHandlerSecure }) => {
     requestHandlerSecure.subscribe(async ({ session, request, route }) => {
-        if (route.secure) {
-            const requestUrl = `${route.host}:${route.port}${route.path}`;
-            let { passphrase, encryptionkey, token } = request.headers;
-            encryptionkey = utils.base64ToString(encryptionkey || "");
-            delete request.headers["passphrase"];
-            delete request.headers["encryptionkey"];
-            delete request.headers["token"];
-            if (session.encryptionkey) {
-                session.encryptionkey.remote = encryptionkey;
-                if (request.data){
-                    requestHandlerSecure.log(`decrypting data received from ${requestUrl}`);
-                    request.data = utils.decryptFromBase64Str(request.data, session.privateKey, session.hashedPassphrase);
-                }
+        if (!route.issecure) {  // this module does not handle unsecure routes
+            return;
+        }
+        let { passphrase, encryptionkey, token } = request.headers;
+        delete request.headers["passphrase"];
+        delete request.headers["encryptionkey"];
+        delete request.headers["token"];
+        if (session.lastRequestId !== request.requestId) { // on 2nd or more request
+            //request id will only be the same as the session request id on first request
+            //this module should have given a token to the requester on first request
+            if (!session.token) {
+                return {
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 401,
+                    statusMessage: "Unauthorised",
+                    data: "session has no authorisation token"
+                };
             }
-            if (passphrase) {
-                const results = utils.hashPassphrase(passphrase, route.hashedPassphraseSalt);
-                if (results.hashedPassphrase ===  route.hashedPassphrase){
-                    requestHandlerSecure.log(`session ${session.Id} is authenticated.`);
-                    const { publicKey, privateKey } = utils.generatePublicPrivateKeys(results.hashedPassphrase);
-                    session.publicKey = publicKey;
-                    session.privateKey = privateKey;
-                    session.token = utils.encryptToBase64Str(utils.getJSONString({ username: session.username, fromhost: session.fromhost, fromport: session.fromport }), publicKey);
-                    session.encryptionkey = {
-                        local: utils.stringToBase64(publicKey),
-                        remote: encryptionkey 
-                    };
-                    session.hashedPassphrase = results.hashedPassphrase;
-                    token = session.token;
+            //need to reassure that the token is still in the headers
+            if (!token) {
+                return {
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 401,
+                    statusMessage: "Unauthorised",
+                    data: "missing authorisation token in request headers"
+                };
+            }
+            if (userSession.token !== token) {
+                return {
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 401,
+                    statusMessage: "Unauthorised",
+                    data: "unauthorised token"
+                };
+            }
+        } else { //On first request for secure route
+            if (!passphrase) {
+                return {
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 401,
+                    statusMessage: "Unauthorised",
+                    data: "missing passphrase in request headers"
+                };
+            }
+            if (!encryptionkey) {
+                return {
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 401,
+                    statusMessage: "Unauthorised",
+                    data: "missing encryptionkey in request headers"
+                };
+            }
+            const results = utils.hashPassphrase(passphrase, route.hashedPassphraseSalt);
+            if (results.hashedPassphrase === route.hashedPassphrase){
+                requestHandlerSecure.log(`session ${session.Id} is authenticated.`);
+                const { publicKey, privateKey } = utils.generatePublicPrivateKeys(results.hashedPassphrase);
+                session.publicKey = publicKey;
+                session.privateKey = privateKey;
+                session.token = utils.encryptToBase64Str(utils.getJSONString({ username: session.username, fromhost: session.fromhost, fromport: session.fromport }), publicKey);
+                session.encryptionkey = {
+                    local: utils.stringToBase64(publicKey),
+                    remote: utils.base64ToString(encryptionkey || "") 
+                };
+                session.hashedPassphrase = results.hashedPassphrase;
+            } else {
+                return {
+                    headers: { 
+                        "Content-Type":"text/plain"
+                    },
+                    statusCode: 401,
+                    statusMessage:"Unauthorised",
+                    data: "invalid cridentials"
+                };
+            }
+        }
+        if (request.data){
+            const requestUrl = `${route.host}:${route.port}${route.path}`;
+            requestHandlerSecure.log(`decrypting data received from ${requestUrl}`);
+            request.data = utils.decryptFromBase64Str(request.data, session.privateKey, session.hashedPassphrase);
+        }
+        const res = await requestHandlerSecure.publish({ session, data: request.data });
+        if (res.headers){
+            res.headers.token = session.token;
+            res.headers.encryptionkey = session.encryptionkey.local;
+            if (res.data && session.encryptionkey.remote){
+                const encryptedData = utils.encryptToBase64Str(res.data, session.encryptionkey.remote);
+                if (encryptedData){
+                    res.data = encryptedData;
                 } else {
                     return {
-                        headers: { 
-                            "Content-Type":"text/plain"
-                        },
-                        statusCode: 401,
-                        statusMessage:"Unauthorised",
-                        data: "passphrase or token required"
+                        headers: { "Content-Type":"text/plain" },
+                        statusCode: 400,
+                        statusMessage:"400 Bad Request",
+                        data: "400 Bad Request failed to encrypt data"
                     };
                 }
-            } else if (!token) {
-                return {
-                    headers: { 
-                        "Content-Type":"text/plain"
-                    },
-                    statusCode: 401,
-                    statusMessage:"Unauthorised",
-                    data: "passphrase or token required"
-                };
-            } else if (session.token !== token) {
-                return {
-                    headers: { 
-                        "Content-Type":"text/plain"
-                    },
-                    statusCode: 401,
-                    statusMessage:"Unauthorised",
-                    data: "passphrase or token required"
-                };
-            }
-            const res = await requestHandlerSecure.publish({ session, data: request.data });
-            if (res.headers){
-                res.headers.token = session.token;
-                res.headers.encryptionkey = session.encryptionkey.local;
-                if (res.data && session.encryptionkey.remote){
-                    const encryptedData = utils.encryptToBase64Str(res.data, session.encryptionkey.remote);
-                    if (encryptedData){
-                        res.data = encryptedData;
-                    } else {
-                        return {
-                            headers: { "Content-Type":"text/plain" },
-                            statusCode: 400,
-                            statusMessage:"400 Bad Request",
-                            data: "400 Bad Request failed to encrypt data"
-                        };
-                    }
-                }
-                return res;
             }
             return res;
+        } else {
+            return {
+                headers: { "Content-Type":"text/plain" },
+                statusCode: 500,
+                statusMessage:"Internal Server Error",
+                data: "calling module responded with no headers"
+            };
         }
     });
 });
